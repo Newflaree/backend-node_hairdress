b63e173106a2bc22e5aa4fc0a0ec1104668ae687
Camilo López <camiloj.lopez79@gmail.com>
2022-10-20 17:59:27 -0300

    Finished register for new users

--- src/services/auth/createUser.service.js
+++ src/services/auth/createUser.service.js
@@ -1,22 +1,32 @@
+const bcrypt = require( 'bcryptjs' );
+// Helpers
+const { generateJWT } = require( '../../helpers/jwt' );
 // Models
-const {generateJWT} = require('../../helpers/jwt');
 const { User } = require( '../../models' );
 
-
+/*
+  DOC: Registers a new user, encrypting their password and generating a jwt to maintain it session
+*/
 const createUserService = async ( name, email, password ) => {
   try {
     const user = new User({ name, email, password });
 
-    // TODO: Encrypt password
-    //
+    // Encrypt password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync( password, salt );
+
     // Save to DB
     await user.save();
 
     // Generate JWT
     const token = await generateJWT( user.uid );
 
+    // TODO: Get manu for frontend
 
-    return { user, token };
+    return {
+      user,
+      token
+    };
 
   } catch ( err ) {
     console.log( `${ '[SERVICE.CREATE-USER]'.red }: Error Detail - ${ err }` );
